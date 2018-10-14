8365c3da96719f6d54b95a58dbb7c37cc40319ed
Nicolas Mahe <nicolas@mahe.me>
2018-10-14 18:09:43 +0700

    Add interface to the task's inputs and outputs

--- template/src/tasks/taskX.ts
+++ template/src/tasks/taskX.ts
@@ -1,4 +1,22 @@
-import { TaskInputs, TaskOutputs } from "mesg-js/lib/service"
+import { TaskInputs, TaskOutputs, TaskOutputCallbackInput } from "mesg-js/lib/service";
+
+interface Inputs extends TaskInputs {
+  foo: string
+  bar: string
+}
+
+interface Outputs extends TaskOutputs {
+  success: (data: OutputsSuccess) => Promise<void>
+  error: (data: OutputsError) => Promise<void>
+}
+
+interface OutputsSuccess extends TaskOutputCallbackInput {
+  x: string
+}
+
+interface OutputsError extends TaskOutputCallbackInput {
+  message: string
+}
 
 const taskX = (inputs: TaskInputs, outputs: TaskOutputs): Promise<void> => {
   if (inputs.foo === "hello" && inputs.bar === "world") {
