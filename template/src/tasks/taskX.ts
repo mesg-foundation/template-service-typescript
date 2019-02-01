89778e727af4b9bbffce363ec60af3514e6bb421
Nicolas Mahe <nicolas@mahe.me>
2019-02-01 11:46:01 +0700

    Use same service definition that the js template

--- template/src/tasks/taskX.ts
+++ template/src/tasks/taskX.ts
@@ -2,8 +2,8 @@ import { TaskInputs, TaskOutputs } from "mesg-js/lib/service"
 
 const taskX = (inputs: TaskInputs, outputs: TaskOutputs): Promise<void> => {
   if (inputs.foo === "hello" && inputs.bar === "world") {
-    return outputs.success({ x: "Hello world is valid" })
+    return outputs.success({ message: "Hello world is valid" })
   }
-  return outputs.error({ message: "invalid inputs" })
+  return outputs.error({ error: "invalid inputs" })
 }
 export { taskX }
